4d9d4dd099f7e25bb699afbf40bb9405d9a1085e
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-11 20:53:53 +0530

    Create fileUpload.js

---
+++ utils/fileUpload.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+dotenv = require('dotenv');
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'fix-track-pro',
+    format: async (req, file) => 'png', // supports promises as well
+    public_id: (req, file) => `${file.fieldname}-${Date.now()}`,
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
