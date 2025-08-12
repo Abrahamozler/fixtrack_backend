0b0d1a69308dca1369c4467bde757e5ecfa04646
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-12 17:03:40 +0530

    Update analysisRoutes.js

--- routes/analysisRoutes.js
+++ routes/analysisRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getServiceAnalysis } = require('../controllers/analysisController.js');
-const { protect, admin } = require('../middleware/authMiddleware.js');
+// NEW: Removed 'admin' from the import
+const { protect } = require('../middleware/authMiddleware.js'); 
 
-// This route will be protected and only accessible by Admins
-router.route('/').get(protect, admin, getServiceAnalysis);
+// NEW: Removed the 'admin' middleware from the route definition
+router.route('/').get(protect, getServiceAnalysis);
 
 module.exports = router;
