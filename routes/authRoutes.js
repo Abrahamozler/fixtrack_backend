dc38d095cb1380121d522d6b01eb54a2218e4965
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-16 22:11:45 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
-// Register new user (first user = Admin, others need referral code)
+// Auth routes
 router.post("/register", registerUser);
-
-// Login existing user
 router.post("/login", loginUser);
 
 module.exports = router;
