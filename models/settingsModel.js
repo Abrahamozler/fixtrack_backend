ddc47b2f56c478eb071c6cf04384642777eb43be
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-17 09:58:22 +0530

    Update settingsModel.js

--- models/settingsModel.js
+++ models/settingsModel.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-// This schema will store a single document with our app settings
 const settingsSchema = new mongoose.Schema({
-  // We use a fixed key to ensure there's only one settings document
   key: { type: String, default: 'app-settings', unique: true },
   staffReferralCode: { type: String, required: true, default: 'DEFAULT-CODE' }
 });
