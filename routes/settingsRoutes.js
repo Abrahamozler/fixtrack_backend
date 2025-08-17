ad5dad797e74ca7ce368eb804c67edd4d6b5fdb5
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-17 10:48:42 +0530

    Update settingsRoutes.js

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getSettings, updateSettings } = require('../controllers/settingsController.js');
-const { protect, admin } = require('../middleware/authMiddleware.js');
+const { getSettings, updateSettings } = require("../controllers/settingsController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
-router.route('/')
-    .get(protect, admin, getSettings)
-    .put(protect, admin, updateSettings);
+router.route("/")
+  .get(protect, admin, getSettings)
+  .put(protect, admin, updateSettings);
 
 module.exports = router;
